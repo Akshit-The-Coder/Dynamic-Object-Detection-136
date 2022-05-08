f546474ed82408f4e8ca1f823a27b3275912be72
Akshit-The-Coder <87372624+Akshit-The-Coder@users.noreply.github.com>
2022-05-08 17:30:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 video = "";
+objects = [];
+status = "";
 
 function preload()
 {
@@ -15,6 +17,22 @@ function setup()
 function draw()
 {
     image(video , 0 , 0 , 480 , 380);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for( i = 0 ; i < objects.length ; i++)
+        {
+            document.getElementById("status").innerHTML = "Status = Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "number of objectsdetected are : " + objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" + objects[i].x + 15 , objects[i].y +15 );
+            noFill();
+            stroke("FF0000");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
 }
 
 function start()
@@ -31,4 +49,14 @@ function modelLoaded()
     video.volume(0);
     video.speed(1);
 
+}
+
+function gotResult(error,results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
